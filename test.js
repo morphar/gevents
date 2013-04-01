4e487e8347a4fef657ca3944a811326779f20669
Dan Larsen <dan@techba.se>
2013-04-01 13:45:33 +0200

    tabs = 2 spaces + renamed main file to index.js

--- test.js
+++ test.js
@@ -5,11 +5,11 @@ var GEvents2 = require('./gevents.js');
 GEvents1.on('bla', function() { console.log('bla', arguments); });
 
 setInterval(function() {
-
-        if(!(process.pid % 2)) {
-            GEvents2.emit('bla', { some: 'data' }, 'bla', 'yadda');
-        } else {
-            GEvents2.globalEmit('bla', { some: 'data2' });
-        }
-
-    }, 2000);
+  
+  if(!(process.pid % 2)) {
+    GEvents2.emit('bla', { some: 'data' }, 'bla', 'yadda');
+  } else {
+    GEvents2.globalEmit('bla', { some: 'data2' });
+  }
+  
+}, 2000);
